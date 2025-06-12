cd8ea6e0c33d3caad0685cd6c6cfd0e6e806c47f
Rigon <rigonmej@gmail.com>
2025-06-12 15:22:19 +0200

    Initial commit

---
+++ app/onboarding.tsx
@@ -0,0 +1,11 @@
+﻿import {StyleSheet, Text, View} from "react-native";
+
+const Onboarding = () => {
+    return (
+        <View>
+            <Text>OnBoarding</Text>
+        </View>
+    )
+}
+export default Onboarding
+const styles = StyleSheet.create({})
