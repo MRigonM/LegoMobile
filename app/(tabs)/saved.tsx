3b24080671e175826ca24070dbd9427eced9a732
Rigon <rigonmej@gmail.com>
2025-06-14 19:30:15 +0200

    Updated UI in Profile and Saved Pages

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,9 +1,18 @@
-﻿import {Text, View} from "react-native";
+﻿import {Image, Text, View} from "react-native";
+import {icons} from "@/constants/icons";
 
 const Saved = () => {
     return (
-        <View>
-            <Text>Saved</Text>
+        <View className={"bg-primary flex-1 px-10"}>
+            <View className={"flex justify-center items-center flex-1 flex-col gap-5"}>
+                <Image source={icons.save}
+                       className={"size-10"}
+                       tintColor={"#Fff"}
+                />
+                <Text className="text-gray-500 text-base">
+                    Saved
+                </Text>
+            </View>
         </View>
     )
 }
